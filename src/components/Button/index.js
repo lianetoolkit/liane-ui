7622792e366dd59a842c23afff8be257521fa695
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-05-10 17:56:29 +0200

    LUI-3 -  adding transition

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,19 +7,20 @@ const Button = ({ children, type, className, style, onClick, size, fluid }) => {
     lg: "py-6 px-7 text-lg",
   };
   const types = {
-    primary: "bg-primary text-white rounded-md",
-    secondary: "text-primary border border-purple-500 bg-white rounded-md",
+    primary: "bg-primary text-white rounded-md hover:bg-purple-900",
+    secondary:
+      "text-primary border border-purple-500 bg-white rounded-md hover:bg-gray-100",
     tertiary: "text-black border-gray border rounded-md",
     ghost: "text-primary bg-transparent",
-    destructive: "bg-red text-white rounded-md",
+    destructive: "bg-red text-white rounded-md hover:bg-red-900",
     inverse: "text-primary bg-white rounded-md",
   };
   return (
     <div
       style={style}
-      className={` ${className ? className : ``} ${
-        size ? sizes[size] : sizes.md
-      } ${type ? types[type] : types.ghost}
+      className={`transition duration-300 ease-in-out ${
+        className ? className : ``
+      } ${size ? sizes[size] : sizes.md} ${type ? types[type] : types.ghost}
       ${fluid ? "block" : "inline-block"} cursor-pointer`}
     >
       {children}
