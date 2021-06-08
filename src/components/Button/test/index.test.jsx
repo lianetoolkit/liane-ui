e3d0e6a54af934939a469bac6a7279d43b599bf7
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-06-08 17:46:21 +0200

    LIU-4 - testing cases

--- src/components/Button/test/index.test.jsx
+++ src/components/Button/test/index.test.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import { expect } from 'chai';
 import { Button } from '../index';
 
 describe('Button', () => {
-  it('should render correctly', () => {
-    const component = shallow(<Button />);
-    expect(component).toMatchSnapshot();
+  it('renders Button components', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('button')).to.have.length(1);
   });
-  it('should render button with given props', () => {
-    const component = shallow(<Button type={'primary'} size={'compact'} />);
-    expect(component).toMatchSnapshot();
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount(<Button onClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
   });
-  it('should be possible to activate button with Spacebar', () => {
-    const component = mount(<Button id="button-test" />);
-    component.find('button#button-test').simulate('keydown', { keyCode: 32 });
-    expect(component).toMatchSnapshot();
-    component.unmount();
+  it('allows us to set props', () => {
+    const wrapper = mount(<Button type="primary" />);
+    expect(wrapper.props().type).to.equal('primary');
+    wrapper.setProps({ size: 'compact' });
+    expect(wrapper.props().size).to.equal('compact');
   });
-  const clickFn = jest.fn();
-  it('button click trigger onclick', () => {
-    const component = shallow(<Button id="button-test-2" onClick={clickFn} />);
-    component.find('button#button-test-2').simulate('click');
-    expect(clickFn).toHaveBeenCalled();
+  it('renders an `button-test-3`', () => {
+    const wrapper = shallow(<Button id="button-test-3" />);
+    expect(wrapper.find('button#button-test-3')).to.have.length(1);
   });
-  // Check also disabled
+  it('renders an `button-test-4 disabled`', () => {
+    const wrapper = shallow(<Button id="button-test-4" disabled />);
+    expect(wrapper.find('button.bg-gray-200')).to.have.lengthOf(1);
+  });
+  // it('renders children when passed in', () => {
+  //   const wrapper = shallow(
+  //     <Button>
+  //       <span className="special">Liane</span>
+  //     </Button>
+  //   );
+  //   expect(wrapper.contains(<span />)).to.equal(true);
+  // });
 });
