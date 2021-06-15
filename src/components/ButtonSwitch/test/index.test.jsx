6b391559194b0cce78a16b33e0e03867f913f73d
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-06-15 19:54:19 +0200

    LUI-4 adding some testing

--- src/components/ButtonSwitch/test/index.test.jsx
+++ src/components/ButtonSwitch/test/index.test.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { ButtonSwitch } from '../index';
+import { expect } from 'chai';
+import { Button } from '../../Button/index';
 
 describe('ButtonSwitch', () => {
-  it('should render correctly', () => {
-    const component = shallow(<ButtonSwitch />);
-    expect(component).toMatchSnapshot();
+  it('renders Button components', () => {
+    const wrapper = shallow(<ButtonSwitch />);
+    expect(wrapper.find('.liu-btn-switch')).to.have.length(1);
+  });
+
+  it('renders children when passed an option', () => {
+    const wrapper = shallow(
+      <ButtonSwitch
+        options={[
+          {
+            label: 'Test',
+            value: 0,
+          },
+        ]}
+      />
+    );
+    expect(wrapper.find('.button-switch')).to.have.lengthOf(1);
+  });
+  it('renders 3 children when passed 3 options', () => {
+    const wrapper = shallow(
+      <ButtonSwitch
+        options={[
+          {
+            label: 'Test 1',
+            value: 0,
+          },
+          {
+            label: 'Test 2',
+            value: 2,
+          },
+          {
+            label: 'Test 3',
+            value: 3,
+          },
+        ]}
+      />
+    );
+    expect(wrapper.find('.button-switch')).to.have.lengthOf(3);
   });
 });
